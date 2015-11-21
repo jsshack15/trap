e23807d0c2b91d5c10b2adafb848fe8d782aadfc
Priya Sharma <priyasharma.26695@gmail.com>
2015-11-22 04:07:33 +0530

    initial commit

---
+++ rashidamon/contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML> 
+<html>
+<head>
+</head>
+<body> 
+
+<form method="post" action="signup.php" method="POST"> 
+   Name: <input type="text" name="name">
+   <br><br>
+   E-mail: <input type="text" name="id">
+   <br><br>
+   Contact: <input type="text" name="ring">
+   <br><br>
+   Suggestion: <textarea name="message" rows="5" cols="40"></textarea>
+   <br><br>
+Location: <input type="text" name="location">
+   
+   <br><br>
+   <input type="submit" name="submit" value="Submit"> 
+</form>
+
+</body>
+</html>
